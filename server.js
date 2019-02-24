9d695459a20679b4bb08119c9a341041df356586
Nikhil Warke <nwarke@protonmail.com>
2019-02-24 12:57:44 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  console.log(req.url);
+  res.send('Hello User');
+});
+
+app.get('/update.bin', (req, res) => {
+  let date = new Date();
+  console.log(date);
+
+  let file = __dirname + '/public/update.bin';
+
+  res.download(file);
+});
+
+app.listen(process.env.PORT);
