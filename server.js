73ecbed113509178e883c2647ec536143c24eac1
Nikhil Warke <nwarke@protonmail.com>
2019-03-17 17:49:35 +0530

    Add firmware file uploader
    It can be used to upload firmware file to rpi

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const app = express();
+const port = process.env.PORT;
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
   console.log(req.url);
-  res.send('Hello User');
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/upload', function(req, res) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "firmware") is used to retrieve the uploaded file
+  let firmware = req.files.firmware;
+
+  if (firmware.name !== 'firmware.bin') {
+    res.send('Filename should be firmware.bin');
+    return;
+  }
+
+  // Use the mv() method to place the file somewhere on your server
+  firmware.mv(__dirname + '/public/update.bin', function(err) {
+    if (err) return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 });
 
 app.get('/update', (req, res) => {
@@ -19,4 +41,6 @@ app.get('/update', (req, res) => {
   res.download(file);
 });
 
-app.listen(process.env.PORT);
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
